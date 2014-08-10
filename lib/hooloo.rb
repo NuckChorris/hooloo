245daee5dac6bd2ed566053d86b1d9a6184302ed
Peter Lejeck <peter.lejeck@gmail.com>
2014-08-10 12:34:45 -0700

    Pull it allllllll in (in lib/hooloo.rb)

--- lib/hooloo.rb
+++ lib/hooloo.rb
@@ -1,5 +1,12 @@
-require "hooloo/version"
+# There has to be a better answer to solving dependencies than this odd order
+# Can we hoist the classes somehow?
 
-module Hooloo
-  # Your code goes here...
-end
+require 'hooloo/hooloo'
+require 'hooloo/mozart_hash'
+require 'hooloo/version'
+require 'hooloo/rollup'
+require 'hooloo/genre'
+require 'hooloo/video'
+require 'hooloo/company'
+require 'hooloo/show'
+#require 'hooloo/search'
